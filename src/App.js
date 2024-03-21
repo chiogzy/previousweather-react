671da2ac86b70f54864d7e6f0e74b302f0dcc52d
chiogzy <63241649+chiogzy@users.noreply.github.com>
2024-03-21 14:50:19 +0100

    Code Updated

--- src/App.js
+++ src/App.js
@@ -27,58 +27,58 @@ export default function App() {
     });
     setSearch(true);
   }
-}
 
-function searchCity() {
-  let apiKey = "197ef3a642b76eef90e131866f74a0a0";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(showTemperature);
-}
+  function searchCity() {
+    let apiKey = "197ef3a642b76eef90e131866f74a0a0";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(showTemperature);
+  }
 
-let form = (
-  <div class="weather-app">
-    <header>
-      <form className="search-form" onSubmit={handleSubmit}>
-        <input
-          className="search-form-input"
-          type="search"
-          placeholder="Enter a City..."
-          onChange={updateCity}
-        />
-        <input
-          className="search-form-button"
-          type="submit"
-          name="Search"
-          value={"search"}
-        />
-      </form>
-    </header>
-  </div>
-);
-
-if (search) {
-  return (
-    <div className="weather-app-data">
-      <strong>{city}</strong>{" "}
-      <div classname="weather-app-details">
-        <p>
-          Thursday, 10:47, condition: {weather.description}, humidity:{" "}
-          <strong> {weather.humidity}%</strong>
-          wind: <strong>{Math.round(weather.wind)} km/h</strong>
-        </p>
-      </div>
-      <div className="weather-app-temperature-container">
-        <span className="weather-app-icon">
-          <img alt="icon" src={weather.icon} />
-        </span>
-        <span class="weather-app-temperature">
-          <strong>{Math.round(weather.temperature)}</strong>
-        </span>
-        <span class="weather-app-unit">°C</span>
-      </div>
-      <Footer />
+  let form = (
+    <div class="weather-app">
+      <header>
+        <form className="search-form" onSubmit={handleSubmit}>
+          <input
+            className="search-form-input"
+            type="search"
+            placeholder="Enter a City..."
+            onChange={updateCity}
+          />
+          <input
+            className="search-form-button"
+            type="submit"
+            name="Search"
+            value={"search"}
+          />
+        </form>
+      </header>
     </div>
   );
-} else {
-  return form;
+
+  if (search) {
+    return (
+      <div className="weather-app-data">
+        <strong>{city}</strong>{" "}
+        <div classname="weather-app-details">
+          <p>
+            Thursday, 10:47, condition: {weather.description}, humidity:{" "}
+            <strong> {weather.humidity}%</strong>
+            wind: <strong>{Math.round(weather.wind)} km/h</strong>
+          </p>
+        </div>
+        <div className="weather-app-temperature-container">
+          <span className="weather-app-icon">
+            <img alt="icon" src={weather.icon} />
+          </span>
+          <span class="weather-app-temperature">
+            <strong>{Math.round(weather.temperature)}</strong>
+          </span>
+          <span class="weather-app-unit">°C</span>
+        </div>
+        <Footer />
+      </div>
+    );
+  } else {
+    return form;
+  }
 }
