c6f0d1857d04e7f9869adedcea51682a41c882c6
chiogzy <63241649+chiogzy@users.noreply.github.com>
2024-03-22 12:08:50 +0100

    apikey updated

--- src/App.js
+++ src/App.js
@@ -29,8 +29,8 @@ export default function App() {
   }
 
   function searchCity() {
-    let apiKey = "197ef3a642b76eef90e131866f74a0a0";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    let apiKey = "fbef01f4et1b02o0d25c27210a43ef3f";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
     axios.get(apiUrl).then(showTemperature);
   }
 
